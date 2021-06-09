d85d0de9e12871e988a16dfc1821998f4ad165a2
maxmakk <90equals50@gmail.com>
2021-06-09 19:11:11 +0300

    finish user interface

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const showForm = document.querySelector(".show-form");
+const form = document.querySelector(".form");
+const overlay = document.querySelector(".overlay");
+const addBook = document.querySelector(".add-book");
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -30,6 +35,40 @@ function addBookToLibrary() {
   // do stuff here
 }
 
+function displaLibrary() {
+  myLibrary.forEach((el) => {
+    console.log(el);
+  });
+}
+
+const container = document.querySelector(".container");
+
+function generateItem() {
+  for (let i = 0; i < 4; i++) {
+    const li = document.createElement("li");
+    const table = document.createElement("table");
+    li.appendChild(table);
+    const tbody = document.createElement("tbody");
+    table.appendChild(tbody);
+    const tr = document.createElement("tr");
+    tbody.appendChild(tr);
+    const th = document.createElement("th");
+    tr.appendChild(th);
+    const td = document.createElement("td");
+    tr.appendChild(td);
+    const trAuthor = document.createElement("tr");
+    tbody.appendChild(trAuthor);
+    const thAuthor = document.createElement("th");
+    trAuthor.appendChild(thAuthor);
+    const tdAuthor = document.createElement("td");
+    trAuthor.appendChild(tdAuthor);
+
+    container.appendChild(li);
+  }
+}
+
+console.log(generateItem());
+
 const book1 = new Book("The sound and the fury", "W.Falknuer", "200", true);
 
 const book2 = new Book("The sound and the fury", "W.Falknuer", "200", false);
@@ -44,3 +83,22 @@ const book6 = new Book("The sound and the fury", "W.Falknuer", "200", false);
 
 myLibrary.push(book1, book2, book3, book4, book5, book6);
 console.dir(myLibrary);
+
+// ***********************************************  Buttons
+
+addBook.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+showForm.addEventListener("click", (e) => {
+  e.preventDefault();
+  form.classList.toggle("closed");
+  overlay.classList.toggle("closed");
+});
+
+overlay.addEventListener("click", () => {
+  form.classList.toggle("closed");
+  overlay.classList.toggle("closed");
+});
+
+displaLibrary();
